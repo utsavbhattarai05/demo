09487e04b6e960cd22840794c03816749fca314e
utsav bhattarai <ubhattarai05@gmail.com>
2023-12-27 11:41:41 +0545

    learing of rails

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  before_action:set_post
+  def create
+    comment= @post.comments.create! params.require(:comment).permit(:content)
+    CommentsMailer.submitted(comment).deliver_later 
+    redirect_to @post
+  end
+  private
+  def set_post
+    @post =Post.find(params[:post_id])
+end
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+end
